2e9100852cfdf86fcfceb68194981fcb712b027d
beovan <85023866+beovan@users.noreply.github.com>
2024-01-26 12:34:34 +0700

    create model ,migrate ,seeders and connect database

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,5 +1,15 @@
-let getHomePage = (req, res) => {
-  return res.render("homepage.ejs");
+import db from "../models/index.js";
+let getHomePage = async (req, res) => {
+  try{
+    let data = await db.User.findAll();
+    return res.render("homepage.ejs", {
+      data: JSON.stringify(data)
+    });
+  }
+  catch(err){
+    console.log(err);
+  }
+
 };
 
 let getAboutPage = (req, res) => {
