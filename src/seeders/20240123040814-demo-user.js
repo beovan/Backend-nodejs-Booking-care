14df2620bcea5d4cf603e601d629562c5c481fe3
beovan <truongvangiap04@gmail.com>
2024-02-27 11:19:44 +0700

    feat: update seeder

--- src/seeders/20240123040814-demo-user.js
+++ src/seeders/20240123040814-demo-user.js
@@ -1,11 +1,17 @@
 'use strict';
 
 module.exports = {
+
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Users', [{
-      firstName: 'John',
-      lastName: 'Doe',
-      email: 'example@example.com',
+      email: 'admin@gmail.com',
+      password: '123456', // plain text
+      firstName: 'BeoVan',
+      lastName: 'Giap',
+      address: 'USA',
+      gender: 1,
+      typeRole: 'ROLE',
+      keyRole: 'R1',
       createdAt: new Date(),
       updatedAt: new Date()
     }]);
