5e2370e403a2cd372615677bd17b68feef6c46f8
beovan <truongvangiap04@gmail.com>
2024-03-25 11:35:11 +0700

    fix: Fix query parameter in handleGetAllUsers function

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -25,7 +25,8 @@ let handleLogin = async ( req , res) => {
 }
 
 let handleGetAllUsers = async (req, res) => {
-    let id = req.body.id; //All ,SINGLE
+    let id = req.query.id; //All ,SINGLE
+
     if(!id){
         return res.status(200).json({
             errCode: 1,
