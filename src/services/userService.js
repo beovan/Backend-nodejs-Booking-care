cce23223721363be7e928c8b1f1816b0bcc78d0b
beovan <truongvangiap04@gmail.com>
2024-04-03 16:15:38 +0700

    Update server.js and userService.js

--- src/services/userService.js
+++ src/services/userService.js
@@ -121,6 +121,7 @@ let createNewUser = (data) => {
           gender: data.gender,
           roleId: data.roleId,
           positionId: data.positionId,
+          image: data.image,
         });
   
         resolve({
@@ -180,7 +181,9 @@ let updateUserData = (data) => {
         user.positionId = data.positionId;
         user.gender = data.gender;
         user.phonenumber = data.phonenumber;
-
+        if (data.avatar) {
+          user.image = data.avatar;
+        }
         await user.save();
 
         resolve({
