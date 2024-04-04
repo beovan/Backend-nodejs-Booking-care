0d5103b6db82598641f028d7cf284bf0540a907e
beovan <truongvangiap04@gmail.com>
2024-04-04 09:39:53 +0700

    Refactor: Allcode and User models

---
+++ src/services/doctorService.js
@@ -0,0 +1,41 @@
+import db from "../models/index";
+
+let getTopDoctorHome = (limitInput) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let users = await db.User.findAll({
+        limit: limitInput,
+        where: { roleId: "R2" },
+        order: [["createdAt", "DESC"]],
+        attrFibutes: {
+          exclude: ["password"],
+        },
+        include: [
+          {
+            model: db.Allcode,
+            as: "positionData",
+            attributes: ["valueVi", "valueEn"],
+          },
+          {
+            model: db.Allcode,
+            as: "genderData",
+            attributes: ["valueVi", "valueEn"],
+          },
+        ],
+        raw: true,
+        nest: true,
+      });
+
+      resolve({
+        errCode: 0,
+        data: users,
+      });
+    } catch (e) {
+        reject(e);
+    }
+  });
+};
+
+module.exports = {
+    getTopDoctorHome: getTopDoctorHome,
+}
