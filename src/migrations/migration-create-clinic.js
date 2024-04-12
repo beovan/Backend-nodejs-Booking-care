a081a63dd49e6a6fb1d1991015d755c2e95f80ae
beovan <truongvangiap04@gmail.com>
2024-04-12 12:27:16 +0700

    Refactor: Update clinic model and migration to include descriptionMarkdown, descriptionHTML, and image fields

--- src/migrations/migration-create-clinic.js
+++ src/migrations/migration-create-clinic.js
@@ -1,11 +1,6 @@
 "use strict";
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    // statusId: DataTypes.STRING,
-    // doctorID: DataTypes.INTEGER,
-    // patientID: DataTypes.INTEGER,
-    // date: DataTypes.DATE,
-    // timeType:DataTypes.STRING
     await queryInterface.createTable("clinics", {
     
       id: {
@@ -20,11 +15,14 @@ module.exports = {
       name: {
         type: Sequelize.STRING,
       },
-      description: {
+      descriptionMarkdown: {
+        type: Sequelize.TEXT,
+      },
+      descriptionHTML: {
         type: Sequelize.TEXT,
       },
       image: {
-        type: Sequelize.STRING,
+        type: Sequelize.BLOB('long'),
       },
       createdAt: {
         allowNull: false,
