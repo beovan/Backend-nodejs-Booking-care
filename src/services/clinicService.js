98c35deff5e03b68459b88f943c5b0c1deb98160
beovan <truongvangiap04@gmail.com>
2024-04-14 16:55:27 +0700

    Refactor: clinicController.js, web.js, and clinicService.js files to add getAllClinic and getDetailClinicById API endpoints

--- src/services/clinicService.js
+++ src/services/clinicService.js
@@ -14,26 +14,84 @@ let createClinic = (data) => {
           errCode: 1,
           errMessage: "Missing required parameters",
         });
-      }
-      else{
+      } else {
         await db.Clinic.create({
-            name: data.name,
-            address: data.address,
-            image: data.imageBase64,
-            descriptionHTML: data.descriptionHTML,
-            descriptionMarkdown: data.descriptionMarkdown
-        })
+          name: data.name,
+          address: data.address,
+          image: data.imageBase64,
+          descriptionHTML: data.descriptionHTML,
+          descriptionMarkdown: data.descriptionMarkdown,
+        });
         resolve({
-            errCode: 0,
-            errMessage: "ok" 
-        })
+          errCode: 0,
+          errMessage: "ok",
+        });
       }
     } catch (e) {
-        reject(e);
+      reject(e);
+    }
+  });
+};
+
+let getAllClinic = () => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let data = await db.Clinic.findAll({});
+      if (data && data.length > 0) {
+        data.map((item) => {
+          item.image = new Buffer(item.image, "base64").toString("binary");
+          return item;
+        });
+      }
+      resolve({
+        errMessage: "ok",
+        errCode: 0,
+      });
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+let getDetailClinicById = (inputId) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      if (!inputId) {
+        resolve({
+          errCode: 1,
+          errMessage: "Missing required parameters",
+        });
+      } else {
+        let data = await db.Clinic.findOne({
+          where: { id: inputId },
+          attributes: [
+            "name",
+            "address",
+            "descriptionHTML",
+            "descriptionMarkdown",
+          ],
+        });
+        if (data) {
+          let doctorClinic = await db.DoctorClinic.findAll({
+            where: { clinicId: inputId },
+            attributes: ["doctorId", "provinceId"],
+          });
+          data.doctorClinic = doctorClinic;
+        } else data = {};
+        resolve({
+          errMessage: "ok",
+          errCode: 0,
+          data,
+        });
+      }
+    } catch (e) {
+      reject(e);
     }
   });
 };
 
 module.exports = {
-    createClinic: createClinic
-}
+  createClinic: createClinic,
+  getAllClinic: getAllClinic,
+  getDetailClinicById: getDetailClinicById,
+};
